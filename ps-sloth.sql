a9d9823e737c01e12df639d66ede7dd7887be727
MoneSuper <mome9132@gmail.com>
2022-07-08 18:43:52 +0300

    database

---
+++ ps-sloth.sql
@@ -0,0 +1,453 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.24-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.0.0.6468
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for ps-slothdb
+CREATE DATABASE IF NOT EXISTS `ps-slothdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ps-slothdb`;
+
+-- Dumping structure for table ps-slothdb.apartments
+CREATE TABLE IF NOT EXISTS `apartments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `citizenid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.apartments: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.bank_accounts
+CREATE TABLE IF NOT EXISTS `bank_accounts` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(250) DEFAULT NULL,
+  `business` varchar(50) DEFAULT NULL,
+  `businessid` int(11) DEFAULT NULL,
+  `gangid` varchar(50) DEFAULT NULL,
+  `amount` bigint(255) NOT NULL DEFAULT 0,
+  `account_type` enum('Current','Savings','Business','Gang') NOT NULL DEFAULT 'Current',
+  PRIMARY KEY (`record_id`),
+  UNIQUE KEY `citizenid` (`citizenid`),
+  KEY `business` (`business`),
+  KEY `businessid` (`businessid`),
+  KEY `gangid` (`gangid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.bank_accounts: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.bank_statements
+CREATE TABLE IF NOT EXISTS `bank_statements` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `account` varchar(50) DEFAULT NULL,
+  `business` varchar(50) DEFAULT NULL,
+  `businessid` int(11) DEFAULT NULL,
+  `gangid` varchar(50) DEFAULT NULL,
+  `deposited` int(11) DEFAULT NULL,
+  `withdraw` int(11) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`record_id`),
+  KEY `business` (`business`),
+  KEY `businessid` (`businessid`),
+  KEY `gangid` (`gangid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.bank_statements: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.bans
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `license` varchar(50) DEFAULT NULL,
+  `discord` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `expire` int(11) DEFAULT NULL,
+  `bannedby` varchar(255) NOT NULL DEFAULT 'LeBanhammer',
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`),
+  KEY `discord` (`discord`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.bans: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.crypto
+CREATE TABLE IF NOT EXISTS `crypto` (
+  `crypto` varchar(50) NOT NULL DEFAULT 'qbit',
+  `worth` int(11) NOT NULL DEFAULT 0,
+  `history` text DEFAULT NULL,
+  PRIMARY KEY (`crypto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.crypto: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.crypto_transactions
+CREATE TABLE IF NOT EXISTS `crypto_transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `message` varchar(50) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.crypto_transactions: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.dealers
+CREATE TABLE IF NOT EXISTS `dealers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `coords` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `time` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `createdby` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.dealers: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.gloveboxitems
+CREATE TABLE IF NOT EXISTS `gloveboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.gloveboxitems: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.houselocations
+CREATE TABLE IF NOT EXISTS `houselocations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `coords` text DEFAULT NULL,
+  `owned` tinyint(1) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `tier` tinyint(4) DEFAULT NULL,
+  `garage` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.houselocations: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.house_plants
+CREATE TABLE IF NOT EXISTS `house_plants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `building` varchar(50) DEFAULT NULL,
+  `stage` varchar(50) DEFAULT 'stage-a',
+  `sort` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `food` int(11) DEFAULT 100,
+  `health` int(11) DEFAULT 100,
+  `progress` int(11) DEFAULT 0,
+  `coords` text DEFAULT NULL,
+  `plantid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `building` (`building`),
+  KEY `plantid` (`plantid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.house_plants: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.lapraces
+CREATE TABLE IF NOT EXISTS `lapraces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `checkpoints` text DEFAULT NULL,
+  `records` text DEFAULT NULL,
+  `creator` varchar(50) DEFAULT NULL,
+  `distance` int(11) DEFAULT NULL,
+  `raceid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `raceid` (`raceid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.lapraces: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.management_funds
+CREATE TABLE IF NOT EXISTS `management_funds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `type` enum('boss','gang') NOT NULL DEFAULT 'boss',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `job_name` (`job_name`),
+  KEY `type` (`type`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.management_funds: ~12 rows (approximately)
+INSERT INTO `management_funds` (`id`, `job_name`, `amount`, `type`) VALUES
+	(1, 'police', 0, 'boss'),
+	(2, 'ambulance', 0, 'boss'),
+	(3, 'realestate', 0, 'boss'),
+	(4, 'taxi', 0, 'boss'),
+	(5, 'cardealer', 0, 'boss'),
+	(6, 'mechanic', 0, 'boss'),
+	(7, 'lostmc', 0, 'gang'),
+	(8, 'ballas', 0, 'gang'),
+	(9, 'vagos', 0, 'gang'),
+	(10, 'cartel', 0, 'gang'),
+	(11, 'families', 0, 'gang'),
+	(12, 'triads', 0, 'gang');
+
+-- Dumping structure for table ps-slothdb.occasion_vehicles
+CREATE TABLE IF NOT EXISTS `occasion_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `seller` varchar(50) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `plate` varchar(50) DEFAULT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `mods` text DEFAULT NULL,
+  `occasionid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `occasionId` (`occasionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.occasion_vehicles: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.phone_gallery
+CREATE TABLE IF NOT EXISTS `phone_gallery` (
+  `citizenid` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table ps-slothdb.phone_gallery: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.phone_invoices
+CREATE TABLE IF NOT EXISTS `phone_invoices` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `amount` int(11) NOT NULL DEFAULT 0,
+  `society` tinytext DEFAULT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `sendercitizenid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.phone_invoices: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.phone_messages
+CREATE TABLE IF NOT EXISTS `phone_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `messages` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `number` (`number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.phone_messages: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.phone_tweets
+CREATE TABLE IF NOT EXISTS `phone_tweets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `firstName` varchar(25) DEFAULT NULL,
+  `lastName` varchar(25) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `date` datetime DEFAULT current_timestamp(),
+  `url` text DEFAULT NULL,
+  `picture` varchar(512) DEFAULT './img/default.png',
+  `tweetId` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.phone_tweets: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.players
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `license` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `money` text NOT NULL,
+  `charinfo` text DEFAULT NULL,
+  `job` text NOT NULL,
+  `gang` text DEFAULT NULL,
+  `position` text NOT NULL,
+  `metadata` text NOT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`citizenid`),
+  KEY `id` (`id`),
+  KEY `last_updated` (`last_updated`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.players: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.playerskins
+CREATE TABLE IF NOT EXISTS `playerskins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `skin` text NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.playerskins: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.player_contacts
+CREATE TABLE IF NOT EXISTS `player_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `iban` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.player_contacts: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.player_houses
+CREATE TABLE IF NOT EXISTS `player_houses` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `house` varchar(50) NOT NULL,
+  `identifier` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `keyholders` text DEFAULT NULL,
+  `decorations` text DEFAULT NULL,
+  `stash` text DEFAULT NULL,
+  `outfit` text DEFAULT NULL,
+  `logout` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `house` (`house`),
+  KEY `citizenid` (`citizenid`),
+  KEY `identifier` (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.player_houses: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.player_mails
+CREATE TABLE IF NOT EXISTS `player_mails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `subject` varchar(50) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `read` tinyint(4) DEFAULT 0,
+  `mailid` int(11) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  `button` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.player_mails: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.player_outfits
+CREATE TABLE IF NOT EXISTS `player_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `outfitname` varchar(50) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `skin` text DEFAULT NULL,
+  `outfitId` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `outfitId` (`outfitId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.player_outfits: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.player_vehicles
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `license` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `vehicle` varchar(50) DEFAULT NULL,
+  `hash` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(50) NOT NULL,
+  `fakeplate` varchar(50) DEFAULT NULL,
+  `garage` varchar(50) DEFAULT NULL,
+  `fuel` int(11) DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `state` int(11) DEFAULT 1,
+  `depotprice` int(11) NOT NULL DEFAULT 0,
+  `drivingdistance` int(50) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  `balance` int(11) NOT NULL DEFAULT 0,
+  `paymentamount` int(11) NOT NULL DEFAULT 0,
+  `paymentsleft` int(11) NOT NULL DEFAULT 0,
+  `financetime` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`),
+  KEY `citizenid` (`citizenid`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.player_vehicles: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.player_warns
+CREATE TABLE IF NOT EXISTS `player_warns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `senderIdentifier` varchar(50) DEFAULT NULL,
+  `targetIdentifier` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `warnId` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.player_warns: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.stashitems
+CREATE TABLE IF NOT EXISTS `stashitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`stash`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.stashitems: ~0 rows (approximately)
+
+-- Dumping structure for table ps-slothdb.trunkitems
+CREATE TABLE IF NOT EXISTS `trunkitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ps-slothdb.trunkitems: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
